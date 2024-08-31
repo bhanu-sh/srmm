35a7725cde106655164a776db45649c8e6ff8fba
bhanu-sh <bhanu1234sharma@gmail.com>
2024-08-31 14:00:24 +0530

    login page ui change

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -57,6 +57,15 @@ export default function SignIn() {
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Welcome to
+            </h2>
+            <h2 className="text-center text-2xl font-bold tracking-tight text-blue-600">
+              SHRI RAJROOP
+            </h2>
+            <h2 className="text-center text-2xl font-bold tracking-tight text-blue-600">
+              MEMORIAL MAHAVIDYALAYA
+            </h2>
+            <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               Sign in to your account
             </h2>
           </div>
