5f9dc357e26128e323d1e0b728cbf33c9dea24da
bhanu-sh <bhanu1234sharma@gmail.com>
2024-09-13 21:50:57 +0530

    Refactor CountCard component to improve code readability and remove unnecessary code duplication

--- src/app/components/CountCard.tsx
+++ src/app/components/CountCard.tsx
@@ -16,32 +16,20 @@ export default function CountCard({
     <div className="w-80 p-4 bg-blue-100 rounded-lg shadow-md my-5">
       <div className="p-4 text-center">
         <h2 className="text-xl  font-semibold">{title}</h2>
-        {isLoading ? (
-          <>
+        <>
+          {isLoading ? (
             <h1 className="text-4xl opacity-50">Loading...</h1>
-            <div className="flex justify-between items-center mt-4">
-              <Link href="" className="mx-auto">
-                <button
-                  disabled
-                  className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400 opacity-50"
-                >
-                  Loading...
-                </button>
-              </Link>
-            </div>
-          </>
-        ) : (
-          <>
+          ) : (
             <h1 className="text-4xl">{String(count)}</h1>
-            <div className="flex justify-between items-center mt-4">
-              <Link href={link} className="mx-auto">
-                <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400">
-                  View
-                </button>
-              </Link>
-            </div>
-          </>
-        )}
+          )}
+          <div className="flex justify-between items-center mt-4">
+            <Link href={link} className="mx-auto">
+              <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400">
+                View
+              </button>
+            </Link>
+          </div>
+        </>
       </div>
     </div>
   );
