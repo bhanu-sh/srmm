c8e3a13ab8e0231983941e02313953df1c0a4cd9
bhanu-sh <bhanu1234sharma@gmail.com>
2024-09-18 12:28:20 +0530

    new user routes

--- src/app/api/user/delete/route.ts
+++ src/app/api/user/delete/route.ts
@@ -7,57 +7,57 @@ export async function POST(request: NextRequest) {
   connect();
 
   try {
-    const token = await getToken({
-      req: request,
-      secret: process.env.NEXTAUTH_SECRET,
-    });
-    if (!token) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
+    // const token = await getToken({
+    //   req: request,
+    //   secret: process.env.NEXTAUTH_SECRET,
+    // });
+    // if (!token) {
+    //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    // }
 
-    const reqBody = await request.json();
-    console.log("Request body:", reqBody);
-    const { user } = reqBody;
+    // const reqBody = await request.json();
+    // console.log("Request body:", reqBody);
+    // const { user } = reqBody;
 
-    if (!Array.isArray(user) || user.length === 0) {
-      return NextResponse.json(
-        { error: "Invalid or empty User array provided" },
-        { status: 400 }
-      );
-    }
+    // if (!Array.isArray(user) || user.length === 0) {
+    //   return NextResponse.json(
+    //     { error: "Invalid or empty User array provided" },
+    //     { status: 400 }
+    //   );
+    // }
 
-    const results = [];
+    // const results = [];
 
-    for (const user_id of user) {
-      try {
-        const userExist = await User.findOne({ _id: user_id });
+    // for (const user_id of user) {
+    //   try {
+    //     const userExist = await User.findOne({ _id: user_id });
 
-        if (!userExist) {
-          results.push({
-            user_id,
-            status: "error",
-            message: "User does not exist",
-          });
-          continue;
-        }
+    //     if (!userExist) {
+    //       results.push({
+    //         user_id,
+    //         status: "error",
+    //         message: "User does not exist",
+    //       });
+    //       continue;
+    //     }
 
-        await User.deleteOne({ _id: user_id });
+    //     await User.deleteOne({ _id: user_id });
 
-        results.push({
-          user_id,
-          status: "success",
-          message: "User deleted successfully",
-        });
-      } catch (error: any) {
-        results.push({
-          user_id,
-          status: "error",
-          message: error.message,
-        });
-      }
-    }
+    //     results.push({
+    //       user_id,
+    //       status: "success",
+    //       message: "User deleted successfully",
+    //     });
+    //   } catch (error: any) {
+    //     results.push({
+    //       user_id,
+    //       status: "error",
+    //       message: error.message,
+    //     });
+    //   }
+    // }
 
-    return NextResponse.json({ data: results });
+    // return NextResponse.json({ data: results });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
--- src/app/api/user/signup/route.ts
+++ src/app/api/user/signup/route.ts
@@ -7,39 +7,38 @@ connect();
 
 export async function POST(request: NextRequest) {
   try {
-    // const reqBody = await request.json()
-    // const { name, phone, password } = reqBody
+    // const reqBody = await request.json();
+    // const { name, phone, password } = reqBody;
 
     // console.log(reqBody);
 
     // // Check if User already exists
-    // const user = await User.findOne({phone})
+    // const user = await User.findOne({ phone });
 
     // if (user) {
-    //     return NextResponse.json({error: 'User already exists'}, {status: 400})
+    //   return NextResponse.json(
+    //     { error: "User already exists" },
+    //     { status: 400 }
+    //   );
     // }
 
     // // Hash password
-    // const salt = await bcrypt.genSalt(10)
-    // const hashedPassword = await bcrypt.hash(password, salt)
+    // const salt = await bcrypt.genSalt(10);
+    // const hashedPassword = await bcrypt.hash(password, salt);
 
     // // Create new User
     // const newUser = new User({
-    //     name,
-    //     phone,
-    //     password: hashedPassword,
-    //     role: 'CollegeAdmin'
-    // })
+    //   name,
+    //   phone,
+    //   password: hashedPassword,
+    //   role: "CollegeAdmin",
+    // });
 
     // // Save User
-    // const savedUser = await newUser.save()
+    // const savedUser = await newUser.save();
     // console.log(savedUser);
 
-    return NextResponse.json({
-      message: "Unauthorized",
-      success: false,
-      // savedUser
-    });
+    return NextResponse.json({ message: "Unauthorised" });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
